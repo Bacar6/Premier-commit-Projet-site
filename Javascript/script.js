a494f8187ca8f65f7541c8410d41dff1d32245f3
gala <andhoumani13003.g@gmail.com>
2022-08-06 15:07:55 +0200

    Premier commit Projet site

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+
+
+function changCouleur(newColor) {
+
+	let elem = document.getElementById("div");
+
+	div.innerHTML = "Sa Marche!!!";
+
+   elem.style.color = newColor; 
+	
+}
+
+console.log(div);
+
+
+
+/*
+const elementDiv = document.getElementById("div");
+
+
+elementDiv.style.color = "green";
+
+
+console.log(elementDiv);
+*/
