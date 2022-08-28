da0fe9b112fba1d13ed914eb80adfccf6b5fca85
gala <andhoumani13003.g@gmail.com>
2022-08-28 12:07:27 +0200

    Exos navBar

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon-Site</title>
 
+  <link href="https://fonts.googleapis.com/css?family=Arya:regular,700" rel="stylesheet" />
+
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     
